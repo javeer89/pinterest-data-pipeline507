d2f23f54c36b7c909a6df56c17a1c8825228dd0a
Javeer Miah <javeermiah@gmail.com>
2024-02-04 06:59:25 +0000

    kinesis streaming script complete however API may have errors and data is not viewable on kinesis data viewer... need to check

---
+++ user_posting_emulation_streaming.py
@@ -0,0 +1,95 @@
+import      requests
+from        time import sleep
+import      random
+from        multiprocessing import Process
+import      boto3
+import      json
+from        sqlalchemy import text, create_engine
+from        datetime import datetime, date, time
+
+
+random.seed(100)
+
+'''PULLS DATA FROM TARGET LOCATION'''
+class       AWSDBConnector:
+
+        def __init__(self):
+
+            self.HOST =         "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
+            self.USER =         'project_user'
+            self.PASSWORD =     ':t%;yCY3Yjg'
+            self.DATABASE =     'pinterest_data'
+            self.PORT =         3306
+            
+        def create_db_connector(self):
+            engine =            create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
+            return engine
+
+
+new_connector = AWSDBConnector()
+
+'''ASSORT DATE-TIME OBJECTS (USED FOR JSON FILE)'''
+
+def datetime_handler(obj):
+    if isinstance(obj, (datetime, date, time)):
+        return str(obj)
+
+
+
+
+'''SEND DATA TO THE KAFKA CLUSTER TOPICS'''
+def put_to_api (invoke_url, result):
+    print('result: :\t', result)
+
+    # invoke url for one record, if you want to put more records replace record with records
+    invoke_url = "https://moyj7yazp4.execute-api.us-east-1.amazonaws.com/stream_test/streams/streaming-0e2a0bfcc015-pin/record"
+
+    payload     =       json.dumps  ({  "StreamName": "YourStreamName",
+                                        "records": [{"value": result}],
+                                        "PartitionKey": "desired-name"                                            
+                                        },  default=datetime_handler)
+
+
+    headers     =       {'Content-Type': 'application/json'}    
+    response    =       requests.request(method="PUT", url=invoke_url, headers=headers, data=payload)
+    print(response.status_code)
+
+
+
+
+
+'''MAIN - INFINITE FUNCTION LOOP'''
+def run_infinite_post_data_loop():
+            while True:
+                sleep(random.randrange(0, 2))
+                random_row = random.randint(0, 11000)
+                engine = new_connector.create_db_connector()
+
+                with engine.connect() as connection:
+
+                    pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
+                    pin_selected_row = connection.execute(pin_string)
+                    for row in pin_selected_row:
+                        pin_result = dict(row._mapping)
+
+                    geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+                    geo_selected_row = connection.execute(geo_string)
+                    for row in geo_selected_row:
+                        geo_result = dict(row._mapping)
+
+                    user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+                    user_selected_row = connection.execute(user_string)
+                    for row in user_selected_row:
+                        user_result = dict(row._mapping)
+                    
+                    
+                    put_to_api("https://moyj7yazp4.execute-api.us-east-1.amazonaws.com/stream_test/streams/streaming-0e2a0bfcc015-pin/record", pin_result)
+                    put_to_api("https://moyj7yazp4.execute-api.us-east-1.amazonaws.com/stream_test/streams/streaming-0e2a0bfcc015-geo/record", geo_result)
+                    put_to_api("https://moyj7yazp4.execute-api.us-east-1.amazonaws.com/stream_test/streams/streaming-0e2a0bfcc015-user/record",user_result)
+
+
+
+
+if __name__ == "__main__":
+    run_infinite_post_data_loop()
+    print('Working')
